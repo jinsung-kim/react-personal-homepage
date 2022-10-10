340a1f320d98b3784f4796b6cd67238a9a604e75
Jin Kim <jinsungkim1210@gmail.com>
2022-10-10 14:14:32 -0700

    v1 done

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 import Navbar from "../components/Navbar";
+import "../styles/Photos.css";
+
+var cache = {};
+
+function importAll(r) {
+    r.keys().forEach((key) => (cache[key] = r(key)));
+}
 
 export default class Photos extends Component {
 
@@ -10,14 +17,23 @@ export default class Photos extends Component {
         this.state = {
             width: props.width
         }
+
+        // Note from the docs -> Warning: The arguments passed to require.context must be literals!
+        importAll(require.context("./photos", false, /\.(png|jpe?g|svg)$/));
     }
 
     render() {
         return (
-            <div className={`about-container${ this.state.width < 500 ? "-small" : ""}`}>
+            <div className={`photos-container${ this.props.width < 500 ? "-small" : ""}`}>
                 <Navbar />
-                <div className="grid-view">
-                    
+                <div className={`grid-view${ this.props.width < 500 ? "-small" : "-large"}`}>
+                {Object.entries(cache).map(module => {
+                    return (
+                        <div style={{ padding: 10, margin: 10 }} key={ module }>
+                            <img style={{width: 100, margin: 'auto', display: 'block'}} src={module[1]} alt="this is "/>
+                        </div>
+                    )
+                })}
                 </div>
             </div>
         );
